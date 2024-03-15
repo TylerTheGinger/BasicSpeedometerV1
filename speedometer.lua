2d1488231752e20d29c06719013916e0be1f0eff
Tyler_TheGinger <60587837+TylerTheGinger@users.noreply.github.com>
2024-03-15 18:32:24 -0400

    Files for Speedometer

---
+++ speedometer.lua
@@ -0,0 +1,25 @@
+function text (content)
+    SetTextFont(1)
+    SetTextProportional(0)
+    SetTextScale(1.9,1.9)
+    SetTextEntry("STRING")
+    AddTextComponentString(content)
+    DrawText(0.9,0.7)
+end
+
+Citizen.CreateThread(function()
+
+    while true do
+        Citizen.Wait(2)
+        --[[
+            kph's factor = 3.6
+            mph's factor = 2.2369
+        ]]
+        local speed = GetEntitySpeed(GetVehiclePedIsIn(GetPlayerPed(-1),false))*2.2369
+        -- Check if the ped is in a vehicle
+        if (IsPedInAnyVehicle(GetPlayerPed(-1), false)) then
+            text(math.floor(speed))
+        end
+    end
+
+end)
